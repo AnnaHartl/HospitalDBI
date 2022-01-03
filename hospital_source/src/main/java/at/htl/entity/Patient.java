2a75fa177a0a9663215c49b35c27fe690d2ef29d
AnnaHartl <anna.sofie.hartl@gmail.com>
2022-01-03 16:17:58 +0100

    added entities Patient and Person

---
+++ hospital_source/src/main/java/at/htl/entity/Patient.java
@@ -0,0 +1,13 @@
+package at.htl.entity;
+
+public class Patient extends Person{
+    private String ssn;
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
---
+++ hospital_source/src/main/java/at/htl/entity/Person.java
@@ -0,0 +1,34 @@
+package at.htl.entity;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+}
