8f8c2be015e74a26a6c5804997bda6726658d358
AnnaHartl <anna.sofie.hartl@gmail.com>
2022-01-19 14:00:35 +0100

    added endpoints for patient

---
+++ hospital_source/src/main/java/at/htl/boundary/PatientResource.java
@@ -0,0 +1,53 @@
+package at.htl.boundary;
+
+import at.htl.control.PatientRepository;
+import at.htl.entity.Patient;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("patient")
+public class PatientResource {
+    private final PatientRepository patientRepository;
+
+    public PatientResource(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    @Path("getAllPatients")
+    public List<Patient> getAllPatients(){
+        return patientRepository.getAllPatients();
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Path("addPatient")
+    public void addPatient(Patient patient){
+        patientRepository.addPatient(patient);
+    }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Path("updatePatient")
+    public Patient updatePatient(Patient patient){
+        return patientRepository.updatePatient(patient);
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @DELETE
+    @Path("deletePatient/{id}")
+    public void deletePatient(@PathParam("id") Long id){
+        patientRepository.deletePatient(id);
+    }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @DELETE
+    @Path("getPatientById/{id}")
+    public Patient getPatientyId(@PathParam("id") Long id){
+        return patientRepository.findPatientById(id);
+    }
+}
