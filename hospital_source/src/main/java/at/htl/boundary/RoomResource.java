b0423026a25ada2159619ec13ceeee1524191b15
HautzmayerSheila <sheilahautzmayerschool@gmail.com>
2022-01-25 13:03:45 +0100

    Endpoint methods added (delete, update add)

--- hospital_source/src/main/java/at/htl/boundary/RoomResource.java
+++ hospital_source/src/main/java/at/htl/boundary/RoomResource.java
@@ -1,12 +1,10 @@
 package at.htl.boundary;
 
 import at.htl.control.RoomRepository;
+import at.htl.entity.Patient;
 import at.htl.entity.Room;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -18,17 +16,41 @@ public class RoomResource {
         this.roomRepository = roomRepository;
     }
 
-    @Path("getAll")
+    @Path("getAllRooms")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Room> getAllRooms(){
         return roomRepository.getAllRooms();
     }
 
-    @Path("findById/{id}")
+    @Path("findRoomById/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Room getRoomById(@PathParam("id") Long id){
         return roomRepository.findRoomById(id);
     }
+
+    @Path("updateRoom")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    public Room updateRoom(Room room){
+        return roomRepository.updateRoom(room);
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @DELETE
+    @Path("deleteRoom/{id}")
+    public void deleteRoom(@PathParam("id") Long id){
+        roomRepository.deleteRoom(id);
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Path("addRoom")
+    public void addRoom(Room room){
+        System.out.println(room.getRoomType());
+        roomRepository.addRoom(room);
+    }
+
 }
