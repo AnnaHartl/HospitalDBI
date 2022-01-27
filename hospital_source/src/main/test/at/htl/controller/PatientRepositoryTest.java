9494f755b93b49eb47e8f6b044213723994dc055
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-27 08:43:16 +0100

    redo test folder

--- hospital_source/src/main/test/at/htl/controller/PatientRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package at.htl.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PatientRepositoryTest {
-
-}
---
+++ hospital_source/src/main/test/java/at/htl/control/RoomRepositoryTest.java
@@ -0,0 +1,7 @@
+package at.htl.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomRepositoryTest {
+
+}
