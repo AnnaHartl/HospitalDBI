36f8771400380b3ba91a7410ac536d6bc4a9e6df
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-27 08:56:14 +0100

    add simple test for RoomRepository

--- hospital_source/src/main/test/java/at/htl/control/RoomRepositoryTest.java
+++ hospital_source/src/main/test/java/at/htl/control/RoomRepositoryTest.java
@@ -1,7 +1,24 @@
 package at.htl.control;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.*;
 
+
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RoomRepositoryTest {
+
+    private final RoomRepository roomRepository;
+
+    RoomRepositoryTest(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Test
+    @Order(1)
+    public void getAllRooms_Test() {
+        var rooms = roomRepository.getAllRooms();
+        org.assertj.core.api.Assertions.assertThat(rooms.size()).isEqualTo(35);
+    }
 
 }
