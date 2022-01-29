a3b035db12e358dfaa17c188d09ea05a6cec1377
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-29 23:52:44 +0100

    add from and to to PatientCondition

--- hospital_source/src/main/java/at/htl/entity/PatientCondition.java
+++ hospital_source/src/main/java/at/htl/entity/PatientCondition.java
@@ -4,11 +4,31 @@ import at.htl.id.PatientConditionId;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
 public class PatientCondition {
     @EmbeddedId
     PatientConditionId id;
+
+    private LocalDateTime fromDateTime;
+    private LocalDateTime toDateTime;
+
+    public LocalDateTime getFromDateTime() {
+        return fromDateTime;
+    }
+
+    public void setFromDateTime(LocalDateTime fromDateTime) {
+        this.fromDateTime = fromDateTime;
+    }
+
+    public LocalDateTime getToDateTime() {
+        return toDateTime;
+    }
+
+    public void setToDateTime(LocalDateTime toDateTime) {
+        this.toDateTime = toDateTime;
+    }
 
     public PatientConditionId getId() {
         return id;
