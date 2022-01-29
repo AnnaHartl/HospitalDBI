182831c1f3668c798c32c7c4b5c3397e60ff0e70
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-29 23:27:37 +0100

    add from and to to BedPatient

--- hospital_source/src/main/java/at/htl/entity/BedPatient.java
+++ hospital_source/src/main/java/at/htl/entity/BedPatient.java
@@ -3,12 +3,17 @@ package at.htl.entity;
 import at.htl.id.BedPatientId;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class BedPatient {
 
     @EmbeddedId
     private BedPatientId id;
+
+    private LocalDateTime fromDateTime;
+    private LocalDateTime toDateTime;
 
     public BedPatientId getId() {
         return id;
