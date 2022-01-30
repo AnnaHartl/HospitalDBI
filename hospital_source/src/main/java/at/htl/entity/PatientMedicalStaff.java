667f77d9dad5dc86ef8f8158caf723e465b5bee7
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-30 01:18:10 +0100

    add from and to date to patientmedicalstaff

--- hospital_source/src/main/java/at/htl/entity/PatientMedicalStaff.java
+++ hospital_source/src/main/java/at/htl/entity/PatientMedicalStaff.java
@@ -5,12 +5,32 @@ import at.htl.id.PatientMedicalStaffId;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "PATIENT_MEDICALSTAFF")
 public class PatientMedicalStaff {
     @EmbeddedId
     PatientMedicalStaffId id;
+
+    private LocalDateTime fromDateTime;
+    private LocalDateTime toDateTime;
+
+    public LocalDateTime getFromDateTime() {
+        return fromDateTime;
+    }
+
+    public void setFromDateTime(LocalDateTime fromDateTime) {
+        this.fromDateTime = fromDateTime;
+    }
+
+    public LocalDateTime getToDateTime() {
+        return toDateTime;
+    }
+
+    public void setToDateTime(LocalDateTime toDateTime) {
+        this.toDateTime = toDateTime;
+    }
 
     public Patient getPatient() {
         return id.getPatient();
