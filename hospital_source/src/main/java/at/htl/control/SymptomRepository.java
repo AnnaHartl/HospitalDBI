d9fef607e656dc6a518e22c01db629aa55800d78
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-31 13:26:20 +0100

    add simple methods to SymptomRepository

--- hospital_source/src/main/java/at/htl/control/SymptomRepository.java
+++ hospital_source/src/main/java/at/htl/control/SymptomRepository.java
@@ -1,7 +1,43 @@
 package at.htl.control;
 
+import at.htl.entity.Symptom;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @ApplicationScoped
 public class SymptomRepository {
+    private final EntityManager em;
+
+    public SymptomRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<Symptom> getAllSymptoms(){
+        return em.createQuery("Select s from Symptom s", Symptom.class)
+                .getResultList();
+    }
+
+    @Transactional
+    public void addSymptom(Symptom symptom){
+        em.persist(symptom);
+    }
+
+    @Transactional
+    public void deleteSymptom(Long id){
+        em.remove(findSymptomById(id));
+    }
+
+    public Symptom findSymptomById(Long id){
+        return em.createQuery("Select s from Symptom s where s.id = :id", Symptom.class)
+                .setParameter("id", id).getResultList()
+                .stream().findFirst().orElse(null);
+    }
+
+    @Transactional
+    public Symptom updateSymptom(Symptom symptom){
+        return em.merge(symptom);
+    }
 }
