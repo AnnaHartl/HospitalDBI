09fe1d4fec864b307eb9e3931907f57796b56547
AnnaHartl <anna.sofie.hartl@gmail.com>
2022-02-01 14:06:46 +0100

    changed patch to put

--- hospital_source/src/main/java/at/htl/boundary/PatientApi.java
+++ hospital_source/src/main/java/at/htl/boundary/PatientApi.java
@@ -1,10 +1,9 @@
 package at.htl.boundary;
 
 import at.htl.control.ConditionRepository;
-import at.htl.control.PatientRepository;
-import at.htl.entity.Condition;
-import at.htl.entity.Patient;
-import at.htl.entity.PatientCondition;
+import at.htl.entity.*;
+import at.htl.service.ConditionService;
+import at.htl.service.PatientService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,19 +11,19 @@ import java.util.List;
 
 @Path("api/patient")
 public class PatientApi {
-    private final PatientRepository patientRepository;
-    private final ConditionRepository conditionRepository;
+    private final PatientService patientService;
+    private final ConditionService conditionService;
 
-    public PatientApi(PatientRepository patientRepository, ConditionRepository conditionRepository) {
-        this.patientRepository = patientRepository;
-        this.conditionRepository = conditionRepository;
+    public PatientApi(PatientService patientService, ConditionService conditionService) {
+        this.patientService = patientService;
+        this.conditionService = conditionService;
     }
 
     @Produces(MediaType.APPLICATION_JSON)
     @GET
     @Path("getAllPatients")
     public List<Patient> getAllPatients(){
-        return patientRepository.getAllPatients();
+        return patientService.getAllPatients();
     }
 
     @Consumes(MediaType.APPLICATION_JSON)
@@ -32,29 +31,29 @@ public class PatientApi {
     @Path("addPatient")
     public void addPatient(Patient patient){
         System.out.println(patient.getFirstName());
-        patientRepository.addPatient(patient);
+        patientService.addPatient(patient);
     }
 
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @PATCH
+    @PUT
     @Path("updatePatient")
     public Patient updatePatient(Patient patient){
-        return patientRepository.updatePatient(patient);
+        return patientService.updatePatient(patient);
     }
 
     @Consumes(MediaType.APPLICATION_JSON)
     @DELETE
     @Path("deletePatient/{id}")
     public void deletePatient(@PathParam("id") Long id){
-        patientRepository.deletePatient(id);
+        patientService.deletePatient(id);
     }
 
     @Produces(MediaType.APPLICATION_JSON)
     @GET
     @Path("getPatientById/{id}")
     public Patient getPatientId(@PathParam("id") Long id){
-        return patientRepository.findPatientById(id);
+        return patientService.findPatientById(id);
     }
 
 
@@ -62,15 +61,20 @@ public class PatientApi {
     @POST
     @Path("addConditionForPatient")
     public PatientCondition addConditionForPatient(@QueryParam("patient") Long patientId,@QueryParam("condition") Long conditionId){
-        Patient p = patientRepository.findPatientById(patientId);
-        Condition c = conditionRepository.findConditionById(conditionId);
+       return patientService.addConditionForPatient(patientId, conditionId);
+    }
 
-        if(p == null)
-            return null;
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Path("addMedicalStaffForPatient")
+    public PatientMedicalStaff addMedicalStaffForPatient(@QueryParam("patient") Long patientId, @QueryParam("medicalstaff") Long medicalStaff){
+        return patientService.addMedicalStaffForPatient(patientId, medicalStaff);
+    }
 
-        if(c == null)
-            return null;
-
-       return patientRepository.addPatientCondition(p, c);
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Path("addMedicalStaffForPatient")
+    public BedPatient addBedForPatient(@QueryParam("patient") Long patientId, @QueryParam("bed") Long bedId){
+        return patientService.addBedForPatient(patientId, bedId);
     }
 }
