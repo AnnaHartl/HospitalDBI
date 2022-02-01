3984342c5cdea0dfc9d2f4625ebaf068389b21c4
AnnaHartl <anna.sofie.hartl@gmail.com>
2022-02-01 14:07:55 +0100

    added Service with crud methods for patient

--- hospital_source/src/main/java/at/htl/control/BedRepository.java
+++ hospital_source/src/main/java/at/htl/control/BedRepository.java
@@ -1,11 +1,15 @@
 package at.htl.control;
 
 import at.htl.entity.Bed;
+import at.htl.entity.BedPatient;
+import at.htl.entity.Patient;
 import at.htl.entity.Room;
+import at.htl.id.BedPatientId;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @ApplicationScoped
@@ -40,5 +44,16 @@ public class BedRepository {
     @Transactional
     public Bed updateBed(Bed bed){
         return entityManager.merge(bed);
+    }
+
+    public BedPatient addBedForPatient(Bed b, Patient p) {
+        BedPatient bp = new BedPatient();
+        BedPatientId id = new BedPatientId();
+        id.setPatient(p);
+        id.setBed(b);
+        bp.setId(id);
+        bp.setFromDateTime(LocalDateTime.now());
+
+        return entityManager.merge(bp);
     }
 }
