8deb94e904084791741b2bb1d8b5ecd1536ac4c7
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-02-03 22:34:14 +0100

    add getSymptomByIdTest

--- hospital_source/src/main/test/java/at/htl/control/SymptomRepositoryTest.java
+++ hospital_source/src/main/test/java/at/htl/control/SymptomRepositoryTest.java
@@ -20,10 +20,30 @@ class SymptomRepositoryTest {
         this.ds = ds;
     }
 
-   @Order(1)
-   @Test
-   public void getAllSymptomsTest(){
+    @Order(1)
+    @Test
+    public void getAllSymptomsTest(){
         var symptoms = symptomRepository.getAllSymptoms();
         org.assertj.core.api.Assertions.assertThat(symptoms.size()).isEqualTo(47);
-   }
+    }
+
+    @Order(2)
+    @Test
+    public void getSymptomByIdTest(){
+        var s1 = symptomRepository.findSymptomById(1L);
+        var s2 = symptomRepository.findSymptomById(47L);
+        var s3 = symptomRepository.findSymptomById(23L);
+
+        org.assertj.core.api.Assertions.assertThat(s1.getName()).isEqualTo("Runny or stuffy nose");
+        org.assertj.core.api.Assertions.assertThat(s1.getId()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(s1.getConditions().size()).isEqualTo(3);
+
+        org.assertj.core.api.Assertions.assertThat(s2.getName()).isEqualTo("Sensation of a lump in your throat");
+        org.assertj.core.api.Assertions.assertThat(s2.getId()).isEqualTo(47);
+        org.assertj.core.api.Assertions.assertThat(s2.getConditions().size()).isEqualTo(1);
+
+        org.assertj.core.api.Assertions.assertThat(s3.getName()).isEqualTo("Itching of the eyes");
+        org.assertj.core.api.Assertions.assertThat(s3.getId()).isEqualTo(23);
+        org.assertj.core.api.Assertions.assertThat(s3.getConditions().size()).isEqualTo(1);
+    }
 }
