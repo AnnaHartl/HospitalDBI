da521084897acdddb0ce002e52b04f108f705b51
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-02-05 13:22:39 +0100

    add updateRoomTest

--- hospital_source/src/main/test/java/at/htl/control/RoomRepositoryTest.java
+++ hospital_source/src/main/test/java/at/htl/control/RoomRepositoryTest.java
@@ -1,7 +1,14 @@
 package at.htl.control;
 
+import at.htl.entity.Room;
+import io.agroal.api.AgroalDataSource;
 import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.*;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.db.api.Assertions.assertThat;
 
 
 @QuarkusTest
@@ -9,9 +16,14 @@ import org.junit.jupiter.api.*;
 class RoomRepositoryTest {
 
     private final RoomRepository roomRepository;
+    private final RoomTypeRepository roomTypeRepository;
 
-    RoomRepositoryTest(RoomRepository roomRepository) {
+    private final AgroalDataSource ds;
+
+    RoomRepositoryTest(RoomRepository roomRepository, RoomTypeRepository roomTypeRepository, AgroalDataSource ds) {
         this.roomRepository = roomRepository;
+        this.roomTypeRepository = roomTypeRepository;
+        this.ds = ds;
     }
 
     @Test
@@ -68,6 +80,30 @@ class RoomRepositoryTest {
         org.assertj.core.api.Assertions.assertThat(room6.getBeds().size()).isEqualTo(1);
         org.assertj.core.api.Assertions.assertThat(room7.getBeds().size()).isEqualTo(1);
         org.assertj.core.api.Assertions.assertThat(room8.getBeds().size()).isEqualTo(4);
+    }
+
+    @Test
+    @Order(3)
+    public void updateRoomTest(){
+        var r = roomRepository.findRoomById(1L);
+        r.setRoomType(roomTypeRepository.findRoomTypeById(2L));
+
+        updateRoom(r);
+
+        Table rT = new Table(ds, "room");
+        assertThat(rT).hasNumberOfRows(35)
+                .row(0)
+                .hasValues(r.getId(),
+                        r.getFloorNumber(),
+                        r.getRoomNumber(),
+                        r.getRoomType().getId(),
+                        r.getStation().getId());
+
+    }
+
+    @Transactional
+    private void updateRoom(Room room){
+        roomRepository.updateRoom(room);
     }
 
 }
