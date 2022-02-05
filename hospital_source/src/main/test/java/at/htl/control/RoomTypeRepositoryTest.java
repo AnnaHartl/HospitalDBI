baacd9d5e40df3b71bb404ae7efd530df2cec901
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-02-05 13:11:28 +0100

    add getRoomTypeByIdTest

--- hospital_source/src/main/test/java/at/htl/control/RoomTypeRepositoryTest.java
+++ hospital_source/src/main/test/java/at/htl/control/RoomTypeRepositoryTest.java
@@ -26,4 +26,21 @@ class RoomTypeRepositoryTest {
         var roomTypes = roomTypeRepository.getAllRoomTypes();
         org.assertj.core.api.Assertions.assertThat(roomTypes.size()).isEqualTo(17);
     }
+
+    @Test
+    @Order(2)
+    public void getRoomTypeByIdTest(){
+        var rt1 = roomTypeRepository.findRoomTypeById(1L);
+        var rt2 = roomTypeRepository.findRoomTypeById(3L);
+        var rt3 = roomTypeRepository.findRoomTypeById(17L);
+
+        org.assertj.core.api.Assertions.assertThat(rt1.getId()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(rt1.getName()).isEqualTo("1-Bedroom");
+
+        org.assertj.core.api.Assertions.assertThat(rt2.getId()).isEqualTo(3);
+        org.assertj.core.api.Assertions.assertThat(rt2.getName()).isEqualTo("3-Bedroom");
+
+        org.assertj.core.api.Assertions.assertThat(rt3.getId()).isEqualTo(17);
+        org.assertj.core.api.Assertions.assertThat(rt3.getName()).isEqualTo("Intensive care surveillance rooms");
+    }
 }
