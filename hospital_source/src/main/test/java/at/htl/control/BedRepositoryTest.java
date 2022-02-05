c1db613f2583f292c86ad5ba3373b2afce26e62b
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-02-05 13:29:02 +0100

    add getBedByIdTest

--- hospital_source/src/main/test/java/at/htl/control/BedRepositoryTest.java
+++ hospital_source/src/main/test/java/at/htl/control/BedRepositoryTest.java
@@ -26,4 +26,24 @@ class BedRepositoryTest {
         var beds = bedRepository.getAllBeds();
         org.assertj.core.api.Assertions.assertThat(beds.size()).isEqualTo(63);
     }
+
+    @Test
+    @Order(2)
+    public void getBedByIdTest(){
+        var b1 = bedRepository.findBedById(1L);
+        var b2 = bedRepository.findBedById(32L);
+        var b3 = bedRepository.findBedById(63L);
+
+        org.assertj.core.api.Assertions.assertThat(b1.getId()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(b1.getBedNumber()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(b1.getRoom().getId()).isEqualTo(1);
+
+        org.assertj.core.api.Assertions.assertThat(b2.getId()).isEqualTo(32);
+        org.assertj.core.api.Assertions.assertThat(b2.getBedNumber()).isEqualTo(32);
+        org.assertj.core.api.Assertions.assertThat(b2.getRoom().getId()).isEqualTo(21);
+
+        org.assertj.core.api.Assertions.assertThat(b3.getId()).isEqualTo(63);
+        org.assertj.core.api.Assertions.assertThat(b3.getBedNumber()).isEqualTo(62);
+        org.assertj.core.api.Assertions.assertThat(b3.getRoom().getId()).isEqualTo(35);
+    }
 }
